0266087e3fa5e51daed3d477d45b85eae411de13
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2025-03-02 22:02:12 +0530

    2.19*: Data for countries, step 2

--- part-2/dataforcountries/src/App.jsx
+++ part-2/dataforcountries/src/App.jsx
@@ -12,7 +12,10 @@ const App = () => {
   }
 
   useEffect(() => {
-    if (input) {
+    if (input.trim() === ''){
+      setCountries([])
+      return;
+    } else if (input) {
       axios
         .get('https://studies.cs.helsinki.fi/restcountries/api/all')
         .then( response  => {
@@ -49,6 +52,10 @@ const App = () => {
     )
   }
 
+  const handleShowCountry = (country) => {
+    setContent(showCountryDetails(country))
+  }
+
   useEffect(() => {
     if (countries.length > 10){
       setContent(<div>Too many matches, specify another filter</div>)
@@ -56,9 +63,10 @@ const App = () => {
       setContent(
         <div>
           {countries.map(country => (
-            <div key={country.name.common}>
-              {country.name.common}
-            </div>
+              <div key={country.name.common}>
+                {country.name.common}{ ' '}
+                <button onClick={() => handleShowCountry(country)}>show</button>
+              </div>
           ))}
         </div>
       )
@@ -79,86 +87,5 @@ const App = () => {
     </div>
   )
 }
-
-
-/*
-
-const App = () => {
-  const [ input, setInput ] = useState('')
-  const [ countries, setCountries ] = useState([])
-  const [content, setContent] = useState(<></>)
-
-  useEffect(() => {
-    if (input.trim() === ""){
-      setCountries([])
-      return;
-    } 
-    axios
-      .get(`https://restcountries.com/v3.1/name/${input}`)
-      .then((response) => {
-        setCountries(response.data)
-      })
-  },[input])
-
-  const handleInputChange = (event) => {
-    setInput(event.target.value)
-  }
-
-  const showCountryDetails = (country) => {
-    return (
-      <div>
-        <h2>{country.name.common}</h2>
-        <p>Capital: {country.capital}</p>
-        <p>Area: {country.area}</p>
-        <h3>Languages:</h3>
-        <ul>
-          {(Object.values(country.languages)).map(lang => 
-            <li key={lang}>
-              {lang}
-            </li>
-          )}
-        </ul>
-        <img src={country.flags.png} alt={country.falgs.alt} />
-      </div>
-    )
-  }
-
-  useEffect(() => {
-    if (countries.length > 10){
-      setContent(<div>Too many matches, specify another filter</div>)
-    } else if (countries.length > 1){
-      setContent(
-        <div>
-          {countries.map(country => {
-            <div key={country.name.common}>
-              {country.name.common}
-            </div>
-          })}
-        </div>
-      )
-    } else if (countries.length === 1){
-      axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countries[0].name.common}`)
-        .then(response => {
-          setContent(showCountryDetails(response.data[0]))
-        })
-    } else {
-      setContent(<></>)
-    }
-
-  },[countries])
-
-
-
-  return (
-    <div>
-      find countries 
-      <input value={input} onChange={handleInputChange}/>
-      <div>
-        {content}
-      </div>
-    </div>
-  )
-}*/
 
 export default App
